bd48ce5edd57307ea4040bd6059e3e0f01f0f7b6
Tollefsen <havard.tollefsen@gmail.com>
2020-01-28 07:36:09 +0100

    Changed to tuxemon sprites and added new keys

--- src/index.js
+++ src/index.js
@@ -1,53 +1,222 @@
 /**
  * Author: Michael Hadley, mikewesthad.com
  * Asset Credits:
- *  - Subtle Patterns
+ *  - Tuxemon, https://github.com/Tuxemon/Tuxemon
  */
 
 const config = {
-	type: Phaser.AUTO,
-	width: 171,
-	height: 160,
-	zoom: 4, // Since we're working with 16x16 pixel tiles, let's scale up the canvas by 4x
-	pixelArt: true, // Force the game to scale images up crisply
-	parent: 'game-container',
-	scene: {
-		preload: preload,
-		create: create
-	}
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: "game-container",
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 }
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
 };
 
 const game = new Phaser.Game(config);
+let cursors;
+let wasd;
+let space;
+let player;
+let showDebug = false;
 
 function preload() {
-	// "this" === Phaser.Scene
-	this.load.image('tiles', '../assets/tilesets/super-mario-tiles.png');
+  this.load.image(
+    "tiles",
+    "../assets/tilesets/tuxmon-sample-32px-extruded.png"
+  );
+  this.load.tilemapTiledJSON("map", "../assets/tilemaps/tuxemon-town.json");
+
+  // An atlas is a way to pack multiple images together into one texture. I'm using it to load all
+  // the player animations (walking left, walking right, etc.) in one image. For more info see:
+  //  https://labs.phaser.io/view.html?src=src/animation/texture%20atlas%20animation.js
+  // If you don't use an atlas, you can do the same thing with a spritesheet, see:
+  //  https://labs.phaser.io/view.html?src=src/animation/single%20sprite%20sheet.js
+  this.load.atlas(
+    "atlas",
+    "../assets/atlas/atlas.png",
+    "../assets/atlas/atlas.json"
+  );
 }
 
 function create() {
-	// Load a map from a 2D array of tile indices
-	// prettier-ignore
-	const level = [
-		[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
-		[  0,  1,  2,  3,  0,  0,  0,  1,  2,  3,  0 ],
-		[  0,  5,  6,  7,  0,  0,  0,  5,  6,  7,  0 ],
-		[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
-		[  0,  0,  0, 14, 13, 14,  0,  0,  0,  0,  0 ],
-		[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
-		[  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
-		[  0,  0, 14, 14, 14, 14, 14,  0,  0,  0, 15 ],
-		[  0,  0,  0,  0,  0,  0,  0,  0,  0, 15, 15 ],
-		[ 35, 36, 37,  0,  0,  0,  0,  0, 15, 15, 15 ],
-		[ 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39 ]
-	];
+  const map = this.make.tilemap({ key: "map" });
 
-	// When loading from an array, make sure to specify the tileWidth and tileHeight
-	const map = this.make.tilemap({ data: level, tileWidth: 16, tileHeight: 16 });
-	const tiles = map.addTilesetImage('tiles');
-	const layer = map.createStaticLayer(0, tiles, 0, 0);
+  // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
+  // Phaser's cache (i.e. the name you used in preload)
+  const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles");
+
+  // Parameters: layer name (or index) from Tiled, tileset, x, y
+  const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
+  const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
+  const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
+
+  worldLayer.setCollisionByProperty({ collides: true });
+
+  // By default, everything gets depth sorted on the screen in the order we created things. Here, we
+  // want the "Above Player" layer to sit on top of the player, so we explicitly give it a depth.
+  // Higher depths will sit on top of lower depth objects.
+  aboveLayer.setDepth(10);
+
+  // Object layers in Tiled let you embed extra info into a map - like a spawn point or custom
+  // collision shapes. In the tmx file, there's an object layer with a point named "Spawn Point"
+  const spawnPoint = map.findObject(
+    "Objects",
+    obj => obj.name === "Spawn Point"
+  );
+
+  // Create a sprite with physics enabled via the physics system. The image used for the sprite has
+  // a bit of whitespace, so I'm using setSize & setOffset to control the size of the player's body.
+  player = this.physics.add
+    .sprite(spawnPoint.x, spawnPoint.y, "atlas", "misa-front")
+    .setSize(30, 40)
+    .setOffset(0, 24);
+
+  // Watch the player and worldLayer for collisions, for the duration of the scene:
+  this.physics.add.collider(player, worldLayer);
+
+  // Create the player's walking animations from the texture atlas. These are stored in the global
+  // animation manager so any sprite can access them.
+  const anims = this.anims;
+  anims.create({
+    key: "misa-left-walk",
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "misa-left-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "misa-right-walk",
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "misa-right-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "misa-front-walk",
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "misa-front-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "misa-back-walk",
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "misa-back-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  const camera = this.cameras.main;
+  camera.startFollow(player);
+  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+  cursors = this.input.keyboard.createCursorKeys();
+  wasd = {
+    left: this.input.keyboard.addKey("a"),
+    right: this.input.keyboard.addKey("d"),
+    down: this.input.keyboard.addKey("s"),
+    up: this.input.keyboard.addKey("w")
+  };
+  space = this.input.keyboard.space;
+
+  // Help text that has a "fixed" position on the screen
+  this.add
+    .text(16, 16, 'Arrow keys to move\nPress "D" to show hitboxes', {
+      font: "18px monospace",
+      fill: "#000000",
+      padding: { x: 20, y: 10 },
+      backgroundColor: "#ffffff"
+    })
+    .setScrollFactor(0)
+    .setDepth(30);
+
+  // Debug graphics
+  this.input.keyboard.once("keydown_F", event => {
+    // Turn on physics debugging to show player's hitbox
+    this.physics.world.createDebugGraphic();
+
+    // Create worldLayer collision graphic above the player, but below the help text
+    const graphics = this.add
+      .graphics()
+      .setAlpha(0.75)
+      .setDepth(20);
+    worldLayer.renderDebug(graphics, {
+      tileColor: null, // Color of non-colliding tiles
+      collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+      faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+    });
+  });
 }
 
 function update(time, delta) {
-	// We aren't using this in the current example, but here is where you can run logic that you need
-	// to check over time, e.g. updating a player sprite's position based on keyboard input
+  const speed = 175;
+  const prevVelocity = player.body.velocity.clone();
+
+  // Stop any previous movement from the last frame
+  player.body.setVelocity(0);
+
+  // Horizontal movement
+  if (cursors.left.isDown || wasd.left.isDown) {
+    player.body.setVelocityX(-speed);
+  } else if (cursors.right.isDown || wasd.right.isDown) {
+    player.body.setVelocityX(speed);
+  }
+
+  // Vertical movement
+  if (cursors.up.isDown || wasd.up.isDown) {
+    player.body.setVelocityY(-speed);
+  } else if (cursors.down.isDown || wasd.down.isDown) {
+    player.body.setVelocityY(speed);
+  }
+
+  // Normalize and scale the velocity so that player can't move faster along a diagonal
+  player.body.velocity
+    .normalize()
+    .scale(cursors.shift.isDown ? speed * 2 : speed);
+
+  // Update the animation last and give left/right animations precedence over up/down animations
+  if (cursors.left.isDown || wasd.left.isDown) {
+    player.anims.play("misa-left-walk", true);
+  } else if (cursors.right.isDown || wasd.right.isDown) {
+    player.anims.play("misa-right-walk", true);
+  } else if (cursors.up.isDown || wasd.up.isDown) {
+    player.anims.play("misa-back-walk", true);
+  } else if (cursors.down.isDown || wasd.down.isDown) {
+    player.anims.play("misa-front-walk", true);
+  } else {
+    player.anims.stop();
+
+    // If we were moving, pick and idle frame to use
+    if (prevVelocity.x < 0) player.setTexture("atlas", "misa-left");
+    else if (prevVelocity.x > 0) player.setTexture("atlas", "misa-right");
+    else if (prevVelocity.y < 0) player.setTexture("atlas", "misa-back");
+    else if (prevVelocity.y > 0) player.setTexture("atlas", "misa-front");
+  }
 }
